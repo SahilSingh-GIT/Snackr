edb8073e7813e2023ca23f760f48928319eb1082
Sahil Singh <sahil2004ara@gmail.com>
2026-08-13 21:27:08 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "https://internship-b7.onrender.com",
-        target: "https://snackr.onrender.com",
+        target: "https://snackr.onrender.com/api",
         changeOrigin: true,
         secure: false,
       },
